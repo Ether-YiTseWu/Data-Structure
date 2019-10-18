d045e2256a3601fb7a1a6b27cc8b4f43a1cf46f4
tailer954 <tailer954@gmail.com>
2019-10-18 15:46:30 +0800

    Stack (Array + Template)

---
+++ Stack_Array/stackTemplate.cpp
@@ -0,0 +1,67 @@
+# include<iostream>
+# define SIZE 10
+using namespace std;
+
+template <class T>
+class stack
+{
+public:
+	T Data[SIZE];
+	T Top;
+	void Push( T );
+	void Pop();
+
+	stack( T ini)
+	{
+		Top = ini;
+	}
+};
+
+template<class T>
+void stack<T>::Push(T i)
+{
+	if (Top >= (SIZE - 1))
+	{
+		cout << "Full" << endl;
+		return;
+	}
+	Top++;
+	Data[Top] = i;
+	cout << "Push : " << i << endl;
+}
+
+template<class T>
+void stack<T>::Pop()
+{
+	if (Top < 0)
+	{
+		cout << "Empty" << endl;
+		return;
+	}
+	cout << "Pop : " << Data[Top] << endl;
+	Top--;
+}
+
+int main()
+{
+	stack<int> st1(-1);		// assign -1 to the "Top" variable
+	stack<char> st2(-1);
+	int i, j;
+
+	for (i = 0; i < 11; i++)
+		st1.Push(i);
+	while (i--)
+		st1.Pop();
+
+	cout << endl;
+	cout << endl;
+	cout << endl;
+
+	for (j = 0; j < 11; j++)
+		st2.Push('A'+ j);	// make it display A ~ J
+	while (j--)
+		st2.Pop();
+
+	system("pause");
+	return 0;
+}
