498a55a63e1fad97a55b68cdf20d4d165d3264d0
tailer954 <44743933+tailer954@users.noreply.github.com>
2019-11-14 19:46:33 +0800

    HanoiTower

--- Recursion/HanoiTower.cpp
+++ Recursion/HanoiTower.cpp
@@ -5,13 +5,13 @@ using namespace std;
 void hannoi(int n, char a, char b, char c)
 {
 	if (n == 1)
-		cout << "Move disk 1 from " << a << " to " << c << endl;
-    else
     {
-        hannoi(n - 1, a, c, b);
-        cout << "Move disk " << n << " from " << a << " to " << c << endl;
-        hannoi(n - 1, b, a, c);
+        cout << "Move disk 1 from " << a << " to " << c << endl;
+        return;
     }
+    hannoi(n - 1, a, c, b);
+    cout << "Move disk " << n << " from " << a << " to " << c << endl;
+    hannoi(n - 1, b, a, c);
 }
 
 int main()
