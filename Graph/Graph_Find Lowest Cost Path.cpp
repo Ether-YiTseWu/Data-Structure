8ce8fd70fdffdb6233b587b90c0ae52bf047b3c9
tailer954 <tailer954@gmail.com>
2019-12-27 16:29:24 +0800

    Graph (Find Lowest Cost Path)

--- Graph/Graph_Find Lowest Cost Path.cpp
+++ Graph/Graph_Find Lowest Cost Path.cpp
@@ -1,181 +1,86 @@
 #include <iostream>
-#include <iomanip>
 using namespace std;
-# define sizeX 12
+
+# define sizeX 13
 # define sizeY 3
 # define size  7
-# define Infinity 99999
+# define Infinity 999
+
 int pathCost[sizeX][sizeY] =
 {
-	{ 1, 2, 17 },
-	{ 1, 5, 20 },
-	{ 1, 6, 4 },
-	{ 1, 7, 24 },
-	{ 2, 3, 6 },
-	{ 2, 4, 8 },
-	{ 2, 7, 12 },
-	{ 3, 4, 11 },
-	{ 4, 5, 19 },
-	{ 4, 7, 15 },
-	{ 5, 6, 9 },
-	{ 5, 7, 32 }
+	{ 1, 2, 4 },
+	{ 1, 3, 6 },
+	{ 1, 4, 6 },
+	{ 2, 3, 1 },
+	{ 2, 5, 7 },
+	{ 3, 5, 6 },
+	{ 3, 6, 4 },
+	{ 4, 3, 2 },
+	{ 4, 6, 5 },
+	{ 5, 3, 6 },
+	{ 5, 7, 6 },
+	{ 6, 5, 1 },
+	{ 6, 7, 8 },
 };
 
-class Node
+class findShortestPath
 {
 public:
-	int Data;
-	Node *next;
-	Node(void)
-	{
-		next = NULL;
-	}
-};
+	int adjacencyMatrix[size][size], startPoint, endPoint;
+	double distance[size];
+	bool visited[size];
 
-class findLowestCostPath
-{
-public:
-	Node nodeConnectCount[size];
-	int graphMatrix[size][size], sortPathCost[sizeX][sizeY], ansPath[size - 1][sizeY], startPoint, endPoint, flag = 0;
-	void buildGraph_Matrix(int*);
-	void printGraph(void);
-	void sortpathCost(int pathCost[sizeX][sizeY]);
-	void findLowestPath(int sortPathCost[sizeX][sizeY]);
-	findLowestCostPath(void)
+	void build_AdjacencyMatrix_mine();
+	void build_AdjacencyMatrix_teacher(int*);
+	void print_AdjacencyMatrix(void);
+	void findPath(void);
+
+	findShortestPath()
 	{
-		// initialize the linked list array data 1~7
 		for (int i = 0; i < size; i++)
-			nodeConnectCount[i].Data = i + 1;
+			visited[i] = 0;
 	}
 };
 
-void findLowestCostPath::sortpathCost(int pathCost[sizeX][sizeY])
-{
-	// Copy pathCost to sortPathCost
-	for (int i = 0; i < sizeX; i++)
-		for(int j = 0; j < sizeY;j++)
-			sortPathCost[i][j] = pathCost[i][j];
-			
-	// bubble sort (sort index and cost)
-	for (int i = 0; i < sizeX; i++)
-	{
-		for (int j = i + 1; j < sizeX; j++)
-		{
-			if (sortPathCost[i][2] > sortPathCost[j][2])
-			{
-				int tmpNode1 = sortPathCost[i][0], tmpNode2 = sortPathCost[i][1], tmpCost = sortPathCost[i][2];
-				
-				sortPathCost[i][0] = sortPathCost[j][0];
-				sortPathCost[j][0] = tmpNode1;
-				sortPathCost[i][1] = sortPathCost[j][1];
-				sortPathCost[j][1] = tmpNode2;
-				sortPathCost[i][2] = sortPathCost[j][2];
-				sortPathCost[j][2] = tmpCost;
-			}
-		}
-	}
-	cout << endl << "sortPathCost" << endl;
-	for (int i = 0; i < sizeX; i++)
-		cout << sortPathCost[i][0] << ' ' << sortPathCost[i][1] << ' ' << sortPathCost[i][2] << endl;
-
-	cout << endl << endl;
-}
-void findLowestCostPath::findLowestPath(int sortPathCost[sizeX][sizeY])
-{
-	int count = 0;
-	
-	// Find the lowest path
-	for (int i = 0; i < sizeX; i++)
-	{
-		int node1, node2;
-		node1 = sortPathCost[i][0];
-		node2 = sortPathCost[i][1];
-
-		// Add elememt to Adjacency linked list
-		for (int j = 0; j < size; j++)
-		{
-			Node *p = &nodeConnectCount[j], *newNode = new Node();
-			if (nodeConnectCount[j].Data == node1)
-			{
-				newNode->Data = node2;
-				while (p->next != NULL)
-					p = p->next;
-				p->next = newNode;
-			}
-			else if (nodeConnectCount[j].Data == node2)
-			{
-				newNode->Data = node1;
-				while (p->next != NULL)
-					p = p->next;
-				p->next = newNode;
-			}
-		}
-
-		// Judge this node ignore/ not ignore (this part is imperfect)
-		Node *q = &nodeConnectCount[node1 - 1];
-		flag = 0;   // initialize flag to zero
-		while(q->next != NULL && flag!= 1)
-		{
-			Node *w = &nodeConnectCount[node2 - 1];
-			while(w->next != NULL && flag!= 1)
-			{
-				if (q->Data == w->Data)
-					flag = 1;
-				else
-					flag = 0;
-				w = w -> next;
-			}
-			q = q -> next;
-		}
-
-		// Continue or find a new path
-		if (flag == 1)
-		{
-			cout << "ignore one path (because connecting like a ring) :  " << node1 << " -> " << node2 << endl;
-			continue;
-		}
-		else if (flag == 0)
-		{
-			ansPath[count][0] = node1;
-			ansPath[count][1] = node2;
-			ansPath[count][2] = sortPathCost[i][2];
-			count++;
-		}
-
-		if (count == 6)
-			break;
-	}
-	
-	// Print ansPath
-	cout << endl << "LowestPath" << endl;
-	for (int i = 0; i < size - 1; i++)
-		cout << ansPath[i][0] << ' ' << ansPath[i][1] << ' ' << ansPath[i][2] << endl;
-	
-	// Print ansPath
-	cout << endl << endl << "Adjacency List" << endl;
-	for (int j = 0; j < size; j++)
-	{
-		Node *p = &nodeConnectCount[j];
-		cout << "Node " << j + 1 << " : ";
-		while (p->next != NULL)
-		{
-			cout << p->Data << "  ";
-			p = p->next;
-		}
-		cout << endl;
-	}
-	cout << endl << endl;
-}
-void findLowestCostPath::buildGraph_Matrix(int* pathCost)
-{
+// Note the parameter *pathCost. Let pathCost[sizeX][sizeY] = pathCost[sizeX*sizeY] 
+void findShortestPath::build_AdjacencyMatrix_mine()
+{	
+	// build adjacencyMatrix, the cost of nodeX to nodeX is 0, others are Infinity 
 	for (int i = 0; i < size; i++)
 	{
 		for (int j = 0; j < size; j++)
 		{
 			if (i == j)
-				graphMatrix[i][j] = 0;
+				adjacencyMatrix[i][j] = 0;
 			else
-				graphMatrix[i][j] = Infinity;
+				adjacencyMatrix[i][j] = Infinity;
+		}
+	}
+
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < size; j++)
+		{
+			for (int k = 0; k < sizeX; k++)
+			{
+				if (pathCost[k][0] == i+1 && pathCost[k][1] == j+1)
+					adjacencyMatrix[i][j] = pathCost[k][2];
+			}
+		}
+	}
+}
+
+void findShortestPath::build_AdjacencyMatrix_teacher(int *pathCost)
+{
+	// build adjacencyMatrix, the cost of nodeX to nodeX is 0, others are Infinity 
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < size; j++)
+		{
+			if (i == j)
+				adjacencyMatrix[i][j] = 0;
+			else
+				adjacencyMatrix[i][j] = Infinity;
 		}
 	}
 
@@ -183,11 +88,12 @@ void findLowestCostPath::buildGraph_Matrix(int* pathCost)
 	{
 		startPoint = pathCost[i * 3];
 		endPoint = pathCost[i * 3 + 1];
-		graphMatrix[startPoint - 1][endPoint - 1] = pathCost[i * 3 + 2];
-		graphMatrix[endPoint - 1][startPoint - 1] = pathCost[i * 3 + 2]; // This graph is bilateral
+		adjacencyMatrix[startPoint - 1][endPoint - 1] = pathCost[i * 3 + 2];
+		adjacencyMatrix[endPoint - 1][startPoint - 1] = pathCost[i * 3 + 2]; // This graph is bilateral
 	}
 }
-void findLowestCostPath::printGraph()
+
+void findShortestPath::print_AdjacencyMatrix()
 {
 	for (int i = 0; i < size; i++)
 	{
@@ -195,25 +101,87 @@ void findLowestCostPath::printGraph()
 		cout << " vex" << tmp;
 
 		for (int j = 0; j < size; j++)
-		if (graphMatrix[i][j] == Infinity)
-			cout << setw(5) << "X";
-		else
-			cout << setw(5) << graphMatrix[i][j];
+			if (adjacencyMatrix[i][j] == Infinity)
+				cout << "    " << "X";
+			else
+				cout << "    " << adjacencyMatrix[i][j];
+
 		cout << endl;
 	}
 }
 
-int main()
+void findShortestPath::findPath(void)
 {
-	findLowestCostPath Path1;
-	Path1.buildGraph_Matrix(&pathCost[0][0]);
-	cout << "頂點  vex1 vex2 vex3 vex4 vex5 vex6 vex7" << endl;
-	Path1.printGraph();
+	// Copy the matrix's first row to distance matrix, and let visited[0] = 1
+	for (int i = 0; i < size; i++)
+		distance[i] = adjacencyMatrix[0][i];
+	visited[0] = 1;
+
+	// print 
+	cout << endl << "Add Node    : 1";
+	cout << endl << "Distance    : ";
+	for (int i = 0; i < size; i++)
+		cout << distance[i] << "    ";
+	cout << endl << "Visted Node : ";
+	for (int i = 0; i < size; i++)
+		cout << visited[i] << "    ";
 	cout << endl;
 
-	Path1.sortpathCost(pathCost);
-	Path1.findLowestPath(Path1.sortPathCost);
+	// Search the shortest path
+	while (1)
+	{
+		int minCost = 1000, distanceTmp[size], minCost_NodeIndex, path[size];
 
+		// find the node we should add and this node's index (陣列中最小且沒有被拜訪過的)
+		for (int i = 0; i < size; i++)
+			if (distance[i] < minCost && visited[i] != 1)
+				minCost = distance[i], minCost_NodeIndex = i;
+		visited[minCost_NodeIndex] = 1;
+
+		// add min cost
+		for (int i = 0; i < size; i++)
+			distanceTmp[i] = adjacencyMatrix[minCost_NodeIndex][i] + minCost;
+		for (int i = 0; i < size; i++)
+			if (distance[i] > distanceTmp[i])
+				distance[i] = distanceTmp[i];
+
+		// print 
+		cout << endl << "Add Node    : " << minCost_NodeIndex + 1;
+		cout << endl << "Distance    : ";
+		for (int i = 0; i < size; i++)
+			cout << distance[i] << "    ";
+		cout << endl << "Visted Node : ";
+		for (int i = 0; i < size; i++)
+			cout << visited[i] << "    ";
+		cout << endl;
+
+		// Break Condition (all nodes have been visited)
+		int sum = 0;
+		for (int i = 0; i < size; i++)
+			sum += visited[i];
+		if (sum == size)
+			break;
+	}
+
+	// print 
+	cout << endl << endl << "The distance of start node to other's node" ;
+	cout << endl << "vex1   vex2   vex3   vex4   vex5   vex6   vex7" << endl;
+	for (int i = 0; i < size; i++)
+		cout << distance[i] << "      ";
+}
+
+int main()
+{
+	int j;
+	findShortestPath P1;
+
+	//P1.build_AdjacencyMatrix_teacher(&pathCost[0][0]);
+	P1.build_AdjacencyMatrix_mine();
+	cout << "頂點  vex1 vex2 vex3 vex4 vex5 vex6 vex7" << endl;
+	P1.print_AdjacencyMatrix();
+	P1.findPath();
+
+	cout << endl << endl;
 	system("pause");
 	return 0;
 }
